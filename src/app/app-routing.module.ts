d0ef1d89890be764731f4425c5c60142f6833fad
laurourbano <74082012+laurourbano@users.noreply.github.com>
2023-01-24 01:32:37 -0300

    tentando estabelecer rotas
    usando o router para estabelecer as rotas da aplicacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  /*
+  { path: '', component: HomeComponent },
+  { pathMatch: 'full', component: HistoricoComponent }
+  */
 ];
 
 @NgModule({
