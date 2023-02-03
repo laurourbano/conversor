8bf6d06e60f5da266ac35070c162c70b5d8e8fad
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2023-02-03 09:21:33 -0300

    Update historico.component.ts

--- src/app/components/historico/historico.component.ts
+++ src/app/components/historico/historico.component.ts
@@ -1,44 +1,33 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { Conversao } from './../../interfaces/conversao';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-historico',
   templateUrl: './historico.component.html',
   styleUrls: [ './historico.component.css' ]
 })
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['data', 'hora', 'moedaSelecionada', 'moedaConvertida', 'valor', 'taxa', 'resultado', 'excluir'];
+  conversoes: any[] = [];
+  dataSource: MatTableDataSource<any>;
 
-export class HistoricoComponent implements Conversao {
-
-
-  displayedColumns: string[] = [ 'data', 'hora', 'moedaSelecionada', 'moedaConvertida', 'valor', 'taxa', 'resultado', 'acoes' ];
-
-  dataSource!: MatTableDataSource<Conversao>;
-
-  @ViewChild(MatSort, { static: true }) sort!: MatSort;
-  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
-
-  conversao!: Conversao;
-  conversoes: Conversao[];
-
-  data!: string;
-  hora!: string;
-  moedaSelecionada!: string;
-  moedaConvertida!: string;
-  valor!: number;
-  taxa!: number;
-  resultado!: number;
-
-  constructor() {
-    this.conversoes = [];
-  };
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   ngOnInit() {
-    this.dataSource = new MatTableDataSource(this.conversoes)
+    const storedConversoes = sessionStorage.getItem('conversoes');
+    if (storedConversoes) {
+      this.conversoes = JSON.parse(storedConversoes);
+    }
+    this.dataSource = new MatTableDataSource(this.conversoes);
     this.dataSource.sort = this.sort;
-  };
+    this.dataSource.paginator = this.paginator;
+  }
 
-
-};
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
