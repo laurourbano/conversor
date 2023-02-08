694833c36ab9d45afd960f18f3295cfb9da0f79c
laurourbano <74082012+laurourbano@users.noreply.github.com>
2023-02-07 22:15:00 -0300

    update
    corrigindo algumas declarações não utilizadas e corigindo erros de console no navegador.

--- src/app/components/delete-confirmation-dialog/delete-confirmation-dialog.component.ts
+++ src/app/components/delete-confirmation-dialog/delete-confirmation-dialog.component.ts
@@ -1,25 +1,32 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  Component,
+  Inject
+} from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-delete-confirmation-dialog',
   templateUrl: './delete-confirmation-dialog.component.html',
-  styleUrls: [ './delete-confirmation-dialog.component.css' ]
+  styleUrls: ['./delete-confirmation-dialog.component.css']
 })
 export class DeleteConfirmationDialogComponent {
-  [ x: string ]: any;
+  historicoComponent: any;
   constructor(
-    public dialogRef: MatDialogRef<DeleteConfirmationDialogComponent>,
+    public dialogRef: MatDialogRef < DeleteConfirmationDialogComponent > ,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
   onYesClick(): void {
-    this[ 'HistoricoComponent' ].deleteData(this.data.item.id).subscribe(() => {
-      this.dialogRef.close();
-    });
-
+    if (this.historicoComponent && this.historicoComponent.deleteItem) {
+      this.historicoComponent.deleteItem(this.data.item.id).subscribe(() => {
+        this.dialogRef.close();
+      });
+    }
   }
 }
